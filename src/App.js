7256dde955bb592873daafd4471e4d347f767570
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-08-11 19:04:40 -0700

    adding Navbar using MUI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExcerciseDetail from './pages/ExcerciseDetail';
+import Footer from './components/Footer';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width={"400px"} sx={{width: {xl: '1488px'}}} m='auto'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/excercise/:id' element={<ExcerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+
   )
 }
 
