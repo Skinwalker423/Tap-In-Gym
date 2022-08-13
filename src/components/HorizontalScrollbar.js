8553dea97673ae9257f2b183f9a473bd87902e97
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-08-12 22:15:16 -0700

    left and right arrows

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React, {useContext} from 'react';
+import { Box, Typography } from '@mui/material';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import BodyPart from './bodyPart';
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
 const HorizontalScrollbar = ({data, bodyPart, setBodyPart}) => {
+
+  const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
 
 
 
   return (
-    <div>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((item) => {
         return (
           <Box 
@@ -20,7 +43,7 @@ const HorizontalScrollbar = ({data, bodyPart, setBodyPart}) => {
           </Box>
         )
       })}
-    </div>
+    </ScrollMenu>
   )
 }
 
