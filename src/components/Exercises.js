e1f13fa6cd5054116deef051d913fa831c39abbc
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-08-14 16:31:25 -0700

    Pagination component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -9,6 +9,20 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
 
   console.log(exercises);
 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const exercisesPerPage = 9;
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise)
+
+  const paginate = (e, value) => {
+      setCurrentPage(value);
+
+      window.scrollTo({top: 1800, behavior: 'smooth'})
+  }
+
   return (
     <Box 
       id='exercises'
@@ -23,11 +37,23 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
         Showing results
       </Typography>
       <Stack direction={'row'} flexWrap='wrap' justifyContent={'center'} sx={{gap: {lg: '110px', xs: '50px'}}}>
-        {exercises.map((exercise, index) => {
+        {currentExercises.map((exercise, index) => {
           return (
             <ExerciseCard key={index} exercise={exercise} />
           )
         })}
+      </Stack>
+      <Stack mt='100px' alignItems='center'>
+        {exercises.length > 9 && (
+          <Pagination
+            color='standard'
+            shape='rounded'
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+           />
+        )}
       </Stack>
     </Box>
   )
