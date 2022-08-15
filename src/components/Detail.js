21e21042ec4e130debdcf29688d54e48a266f120
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-08-15 16:06:23 -0700

    refactoring detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -12,29 +12,44 @@ const Detail = ({exerciseDetail}) => {
     console.log(gifUrl);
     console.log(exerciseDetail);
 
+    const extraDetail = [
+        {
+            icon: BodyPartImage,
+            name: bodyPart
+        },
+        {
+            icon: TargetImage,
+            name: target
+        },
+        {
+            icon: EquipmentImage,
+            name: equipment
+        },
+    ]
+
         
     return (
         <Stack gap='60px' sx={{flexDirection: {lg: 'row'}, p: '20px', alignItems: 'center'}}>
             <img src={gifUrl} alt={name} loading='lazy' className='detail-image' />
             <Stack>
-                <Stack sx={{flexDirection: {lg: 'row'}, p: '20px', alignItems: 'center'}} >
-                    <img src={BodyPartImage} alt={bodyPart} />
-                    <Typography>
-                        {bodyPart}
+                <Stack sx={{gap: {lg: '35px', xs: '20px'}}}>
+                    <Typography variant='h3'>
+                        {name}
+                    </Typography>
+                    <Typography variant='h6'>
+                        Exercises keep you strong. {`${name} `} is one of the best exercises to target your {target}. It will help you improve your mood and gain energy.
                     </Typography>
                 </Stack>
-                <Stack sx={{flexDirection: {lg: 'row'}, p: '20px', alignItems: 'center'}} >
-                    <img src={TargetImage} alt={target} />
+                {extraDetail.map(({icon, name}) => (
+                    <Stack key={name} direction='row' gap='24px' alignItems={'center'} mt='20px' >
+                    <Button sx={{background: '#fff', borderRadius: '50%'}}>
+                        <img src={icon} alt={name} />
+                    </Button>
                     <Typography>
-                        {target}
+                        {name}
                     </Typography>
                 </Stack>
-                <Stack sx={{flexDirection: {lg: 'row'}, p: '20px', alignItems: 'center'}} >
-                    <img src={EquipmentImage} alt={equipment} />
-                    <Typography>
-                        {equipment}
-                    </Typography>
-                </Stack>
+                ))}
             </Stack>
         </Stack>
         
